dc8c6764cb9b4f9ef214711712ac3d813addcd59
Damon R <dmarcr1997@gmail.com>
2020-01-23 22:39:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(words)
     rev_words = ""
     new_word = words.split(" ")
     new_word.each_with_index do |item, index|
-      if index == item.count() -1
+      if index == words.count() -1
         rev_words << "#{item.reverse()}"  
       else
         rev_words << "#{item.reverse()} "  
